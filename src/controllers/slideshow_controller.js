b445ffe30a5925a4584046de801a1fec5b9a9adc
kazuto ando <kazuto.ando@gmail.com>
2025-02-05 14:21:59 +0900

    add function slideshow

---
+++ src/controllers/slideshow_controller.js
@@ -0,0 +1,28 @@
+import {Controller} from "@hotwired/stimulus";
+
+export default class extends Controller {
+    static targets = ["slide"];
+
+    initialize() {
+        this.showCurrentSlide()
+    }
+
+    next(){
+        this.index++
+    }
+
+    showCurrentSlide(){
+        this.slideTargets.forEach((el,i) => {
+            el.classList.toggle("slide--current", this.index === i)
+        })
+    }
+
+    get index(){
+        return parseInt(this.data.get("index"))
+    }
+
+    set index(value){
+        this.data.set("index", value);
+        this.showCurrentSlide()
+    }
+}
